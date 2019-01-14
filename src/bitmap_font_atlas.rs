a3f4cb51bfb4c4fb6900cc0cac5e18c43488217d
LambdaXymox <lambda.xymox@gmail.com>
2019-01-13 19:48:00 -0600

    Add a function to write out bmfa file to a writer instead of just a file.

--- src/bitmap_font_atlas.rs
+++ src/bitmap_font_atlas.rs
@@ -4,7 +4,6 @@ use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::Read;
-use std::io::Write;
 use std::path::Path;
 
 use image::png;
@@ -208,15 +207,9 @@ pub fn write_atlas_buffer<P: AsRef<Path>>(atlas: &BitmapFontAtlas, path: P) -> i
     )
 }
 
-///
-/// Write the bitmap font atlas to the disk.
-///
-pub fn write_font_atlas<P: AsRef<Path>>(atlas: &BitmapFontAtlas, path: P) -> io::Result<()> {
-    // Set up the image archive.
-    let mut file_path = path.as_ref().to_path_buf();
-    file_path.set_extension("bmfa");
-    let mut file = File::create(&file_path)?;
-    let mut zip_file = zip::ZipWriter::new(file);
+
+pub fn write_to_writer<W: io::Write + io::Seek>(writer: W, atlas: &BitmapFontAtlas) -> io::Result<()> {
+    let mut zip_file = zip::ZipWriter::new(writer);
     let options =
         zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
 
@@ -234,4 +227,16 @@ pub fn write_font_atlas<P: AsRef<Path>>(atlas: &BitmapFontAtlas, path: P) -> io:
     zip_file.finish()?;
 
     Ok(())
+}
+
+///
+/// Write the bitmap font atlas to the disk.
+///
+pub fn write_font_atlas<P: AsRef<Path>>(atlas: &BitmapFontAtlas, path: P) -> io::Result<()> {
+    // Set up the image archive.
+    let mut file_path = path.as_ref().to_path_buf();
+    file_path.set_extension("bmfa");
+    let mut file = File::create(&file_path)?;
+
+    write_to_writer(file, atlas)
 }
